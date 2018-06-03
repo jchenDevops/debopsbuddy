8156f9fe4d9830a21ce259a800dc3536b643d346
JamesChen <jms_chn@yahoo.com>
2018-06-03 11:00:55 -0400

    about component added

---
+++ src/main/java/com/devopsbuddy/web/controllers/advices/CopyController.java
@@ -0,0 +1,16 @@
+package com.devopsbuddy.web.controllers.advices;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class CopyController {
+	
+	@RequestMapping("/about")
+	
+	public String about() {
+		return "copy/about"; 
+	}
+
+}
